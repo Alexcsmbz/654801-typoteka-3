523d039ca1102962d6c3345172106356687768c5
Aleksandr Bautin <Aleksandr.Bautin@x5.ru>
2021-08-06 23:03:50 +0300

    *

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,43 +1,26 @@
 'use strict';
 
 const fs = require(`fs`);
-const {
-  MAX_ANNOUNCE_SENTENCES_AMOUNT,
-  TITLES,
-  SENTENCES,
-  CATEGORIES,
-  MAX_MONTHS_PERIOD,
-} = require(`./constants`);
+const {MAX_ANNOUNCE_SENTENCES_AMOUNT, TITLES, SENTENCES, CATEGORIES, MAX_MONTHS_PERIOD} = require(`./constants`);
 const {DEFAULT_AMOUNT, FILE_NAME, MAX_ADS_AMOUNT} = require(`./constants`);
 const {getRandomDate, getRandomInt, shuffle} = require(`./utils`);
-const {ExitCode} = require(`../../constants`);
+const {ExitCode} = require(`src/constants`);
 
-const generateAds = (count) => Array(count)
-        .fill({})
-    .map(() => ({
-      title: TITLES[getRandomInt(0, TITLES.length - 1)],
-      announce: shuffle(SENTENCES)
-        .slice(0, getRandomInt(1, MAX_ANNOUNCE_SENTENCES_AMOUNT))
-        .join(` `),
-      fullText: shuffle(SENTENCES)
-        .slice(0, SENTENCES.length - 1)
-        .join(` `),
-      createdDate: getRandomDate(
-          new Date(
-              new Date().getFullYear(),
-              new Date().getMonth() - MAX_MONTHS_PERIOD,
-              1
-          ),
-          new Date()
-      ),
-      сategory: shuffle(CATEGORIES).slice(
-          0,
-          getRandomInt(1, CATEGORIES.length - 1)
-      ),
-    }));
+const generateAds = (/** @type {number} */ count) => Array(count).fill({}).map(() => ({
+  title: TITLES[getRandomInt(0, TITLES.length - 1)],
+  announce: shuffle(SENTENCES).slice(0, getRandomInt(1, MAX_ANNOUNCE_SENTENCES_AMOUNT)).join(` `),
+  fullText: shuffle(SENTENCES).slice(0, SENTENCES.length - 1).join(` `),
+  createdDate: getRandomDate(
+      new Date(new Date().getFullYear(), new Date().getMonth() - MAX_MONTHS_PERIOD, 1), new Date(),
+  ),
+  сategory: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length - 1)),
+}));
 
 module.exports = {
   name: `--generate`,
+  /**
+   * @param {string[] | [any]} [args]
+   */
   run(args) {
     const [amount] = args;
     const amountAd = Number.parseInt(amount, 10) || DEFAULT_AMOUNT;
