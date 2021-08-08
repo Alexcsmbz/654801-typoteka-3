ee69e78139c39ec165be61972c78e4953a8d98f2
Aleksandr Bautin <Aleksandr.Bautin@x5.ru>
2021-08-08 21:49:08 +0300

    fixed consistent flow to parallel flow

--- src/service/cli/constants.js
+++ src/service/cli/constants.js
@@ -5,9 +5,7 @@ const MAX_ADS_AMOUNT = 1000;
 const MAX_MONTHS_PERIOD = 3;
 const FILE_NAME = `mocks.json`;
 const MAX_ANNOUNCE_SENTENCES_AMOUNT = 5;
-const FILE_SENTENCES_PATH = `./data/sentences.txt`;
-const FILE_TITLES_PATH = `./data/titles.txt`;
-const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const mockFilePaths = [`./data/sentences.txt`, `./data/titles.txt`, `./data/categories.txt`];
 
 module.exports = {
   MAX_ADS_AMOUNT,
@@ -15,7 +13,5 @@ module.exports = {
   FILE_NAME,
   MAX_ANNOUNCE_SENTENCES_AMOUNT,
   MAX_MONTHS_PERIOD,
-  FILE_SENTENCES_PATH,
-  FILE_TITLES_PATH,
-  FILE_CATEGORIES_PATH,
+  mockFilePaths,
 };
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -4,9 +4,7 @@ const fs = require(`fs`).promises;
 const {
   MAX_ANNOUNCE_SENTENCES_AMOUNT,
   MAX_MONTHS_PERIOD,
-  FILE_SENTENCES_PATH,
-  FILE_TITLES_PATH,
-  FILE_CATEGORIES_PATH,
+  mockFilePaths,
 } = require(`./constants`);
 const {DEFAULT_AMOUNT, FILE_NAME, MAX_ADS_AMOUNT} = require(`./constants`);
 const {getRandomDate, getRandomInt, shuffle, readContent} = require(`./utils`);
@@ -28,6 +26,11 @@ const generateAds = (
   сategory: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
 }));
 
+const doParallelFlow = async (
+    /** @type  {any[]} */ items,
+    /** @type {(item: any) => Promise<any>} */ getItem,
+) => await Promise.all(items.map(async (item) => await getItem(item)));
+
 module.exports = {
   name: `--generate`,
   /**
@@ -36,9 +39,7 @@ module.exports = {
   async run(args) {
     const [amount] = args;
     const amountAd = Number.parseInt(amount, 10) || DEFAULT_AMOUNT;
-    const sentences = await readContent(FILE_SENTENCES_PATH);
-    const titles = await readContent(FILE_TITLES_PATH);
-    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const [sentences, titles, categories] = await doParallelFlow(mockFilePaths, readContent);
 
     const content = JSON.stringify(generateAds(amountAd, titles, categories, sentences));
 
